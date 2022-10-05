86028a7627115c4ae303c53162dd7b808b35ad6e
LauraCH27 <lchincapiear@cesde.net>
2022-10-05 15:58:15 -0500

    proyecto siembras octubre 5

--- modulo.py
+++ modulo.py
@@ -1,1 +1,6 @@
+import pandas as pd
+
 #funtion en python para optimizar mi codigo 
+def crearTabla():
+    tabla=pd.read_csv('./Siembras.csv')
+    return tabla
---
+++ modulo2.py
@@ -0,0 +1,8 @@
+from cgitb import html
+
+
+def generarHTML(tabla,nombreArchivo):
+    html=tabla.to_html()
+    archivo=open(nombreArchivo,"w",encoding="utf-8")
+    archivo.write(html)
+    archivo.close()
